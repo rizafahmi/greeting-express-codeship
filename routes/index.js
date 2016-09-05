7177e970b6916700f7c702ffd4784964f6b5503e
Riza Fahmi <rizafahmi@gmail.com>
2016-09-05 21:24:23 +0700

    Greeting finish

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+import greeting from '../greeting'
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', greeting: greeting(new Date) });
 });
 
 module.exports = router;

a77a93c1ed863a1a675b80d33772630815ec53e3
Riza Fahmi <rizafahmi@gmail.com>
2016-09-05 21:22:39 +0700

    Add greeting module

---
+++ greeting.js
@@ -0,0 +1,13 @@
+module.exports = function(time) {
+
+  var hours = time.getHours()
+  if (hours < 4 || hours > 18) {
+    return "Selamat malam"
+  } else if (hours < 12) {
+    return "Selamat pagi"
+  } else if (hours < 15) {
+    return "Selamat siang"
+  } else {
+    return "Selamat sore"
+  }
+}

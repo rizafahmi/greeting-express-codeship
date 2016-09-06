b62eef8dc1239f2c5629a1b1a40d4b4c4e86ed1d
Riza Fahmi <rizafahmi@gmail.com>
2016-09-06 10:45:46 +0700

    Don't forget to change the test case as well ya

--- test/greeting_spec.js
+++ test/greeting_spec.js
@@ -2,27 +2,27 @@ import {expect} from 'chai'
 import greeting from '../greeting'
 
 describe('check for time', () => {
-  it('should say "Selamat pagi" at 4am', () => {
+  it('should say "Good morning" at 4am', () => {
     let fourAm = new Date()
     fourAm.setHours(4)
-    expect(greeting(fourAm)).to.equal("Selamat pagi")
+    expect(greeting(fourAm)).to.equal("Good morning")
   })
 
-  it('should say "Selamat siang" at 12pm', () => {
+  it('should say "Good afternoon" at 12pm', () => {
     let twelvePM = new Date()
     twelvePM.setHours(12)
-    expect(greeting(twelvePM)).to.equal("Selamat siang")
+    expect(greeting(twelvePM)).to.equal("Good afternoon")
   })
 
-  it('should say "Selamat sore" at 4pm', () => {
+  it('should say "Good evening" at 4pm', () => {
     let fourPm = new Date()
     fourPm.setHours(16)
-    expect(greeting(fourPm)).to.equal("Selamat sore")
+    expect(greeting(fourPm)).to.equal("Good evening")
   })
 
-  it('should say "Selamat malam" at 8pm', () => {
+  it('should say "Good night" at 8pm', () => {
     let eightPm = new Date()
     eightPm.setHours(20)
-    expect(greeting(eightPm)).to.equal("Selamat malam")
+    expect(greeting(eightPm)).to.equal("Good night")
   })
 })
